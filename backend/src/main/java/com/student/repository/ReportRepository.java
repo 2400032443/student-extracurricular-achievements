10bb503deabdb412ccb4408d2a3272e41082f168
2400032443 <2400032443@kluniversity.in>
2026-04-08 01:07:04 +0530

    Add ReportRepository interface implementing JpaRepository

---
+++ backend/src/main/java/com/student/repository/ReportRepository.java
@@ -0,0 +1,11 @@
+package com.student.repository;
+
+import com.student.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findAllByActiveTrue();
+}
