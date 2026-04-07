8dd599b49238aa34dc3da7bb6f448370af453334
2400032443 <2400032443@kluniversity.in>
2026-04-08 00:51:21 +0530

    Add StudentExtracurricularAchievementsApplication.java file

---
+++ backend/src/main/java/com/student/StudentExtracurricularAchievementsApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentExtracurricularAchievementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentExtracurricularAchievementsApplication.class, args);
+    }
+
+}
